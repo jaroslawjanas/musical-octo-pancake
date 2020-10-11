2b9ba88fc6f17bbf62e7550e83577a1b833ffc0c
unknown <ertagonn@gmail.com>
2020-10-11 14:03:04 +0100

    Added student age test

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,16 +1,27 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.joda.time.DateTime;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class StudentTest {
-    private final Student mockStudent = new Student();
+    private Student mockStudent;
 
-    StudentTest () {
-        mockStudent.setName("Josh");
-        mockStudent.setAge(21);
+    @BeforeEach
+    public void init(){
+       mockStudent = new Student();
+       mockStudent.setName("Josh");
+       mockStudent.setDob(new DateTime(1998, 12, 26, 12, 0, 0, 0));
+       mockStudent.setNow(new DateTime(2020, 1, 30, 12, 0 ,0 ,0));
     }
 
     @Test
     public void studentGetUsername() {
         assertEquals("Josh21", mockStudent.getUsername());
+    }
+
+    @Test
+    public void studentGetAge() {
+        assertEquals(21 ,mockStudent.getAge());
     }
 }
