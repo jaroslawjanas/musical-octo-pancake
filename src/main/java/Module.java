d3ee3ef52c5d7a38d36f54c343ca5e177f411766
jaroslawjanas <jaroslawjanas.dev@gmail.com>
2022-11-17 18:15:14 +0100

    Added remaning getters/setters Improved Student's constructors

--- src/main/java/Module.java
+++ src/main/java/Module.java
@@ -4,6 +4,15 @@ public class Module {
     private String name;
     private String id;
     private ArrayList<Student> students = new ArrayList<Student>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     private ArrayList<Course> courses = new ArrayList<Course>();
 
     Module(){
@@ -18,4 +27,19 @@ public class Module {
         this.name = name;
     }
 
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(ArrayList<Course> courses) {
+        this.courses = courses;
+    }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,5 +1,9 @@
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Student {
 
@@ -11,8 +15,43 @@ public class Student {
     private ArrayList<Module> modules = new ArrayList<Module>();
     private ArrayList<Course> courses = new ArrayList<Course>();
 
-    Student() {
+    Student(){}
 
+    Student(
+            String name,
+            Integer age,
+            DateTime dob, // Date Of Birth
+            Integer id,
+            String username,
+            ArrayList<Module> modules,
+            ArrayList<Course> courses  // a student can't be in multiple courses, but I'm allowing for flexibility
+    ) {
+        this.name = name;
+        this.age = age;
+        this.dob = dob;
+        this.id = id;
+        this.username = username;
+        this.modules =modules;
+    }
+
+    Student(
+            String name,
+            Integer age,
+            String dob,
+            Integer id,
+            String username,
+            Module[] modules,
+            Course course
+    ) {
+        this.name = name;
+        this.age = age;
+        // Not handling other formats since it's outside of the scope of the assignment
+        this.dob = DateTime.parse(dob, DateTimeFormat.forPattern("dd/MM/yyyy"));
+        this.id = id;
+        this.username = username;
+        this.modules = new ArrayList<Module>(Arrays.asList(modules));
+        this.courses = new ArrayList<Course>();
+        this.courses.add(course);
     }
 
     public String getName() {
@@ -22,4 +61,53 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public DateTime getDob() {
+        return dob;
+    }
+
+    public void setDob(DateTime dob) {
+        this.dob = dob;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public ArrayList<Module> getModules() {
+        return modules;
+    }
+
+    public void setModules(ArrayList<Module> modules) {
+        this.modules = modules;
+    }
+
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(ArrayList<Course> courses) {
+        this.courses = courses;
+    }
+
 }
